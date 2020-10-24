8aa656d1cdb7bcfd2dba4bdcc0fd16896cc78fb6
Reza Fahlevi <rezafahleviex@gmail.com>
2020-10-25 00:51:08 +0700

    master

--- app/view/main/dataview.js
+++ app/view/main/dataview.js
@@ -30,10 +30,10 @@ Ext.define('myapp.view.main.dataview', {
             scrollable: 'y',
             cls: 'dataview-basic',
             itemTpl: '<div class="card" style="width:300px">' +
-            '<img class="card-img-top" style="width:30% height:auto" src="{gambar}">' +
+            '<img class="card-img-top" style="width:20% height:auto" src="{gambar}">' +
             '<div class="card-body">' +
             '<h2 class="card-title">Nama: {name}</h2>' +
-            '<p class="card-text">Email: {email}</p>' +
+            '<p class="card-text">Email: {email}</p>' + 
             '<p class="card-text">Phone: {phone}</p><br>' +
             '<button class="btn btn-primary">Detail</button>' +
             '</div>' +
